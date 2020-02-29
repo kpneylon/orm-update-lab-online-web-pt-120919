65756530fbfe2703dd65383b94ef6040e89d5c95
Kenneth Neylon <kenneth_neylon@hotmail.com>
2020-02-29 02:59:38 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -8,5 +8,15 @@ class Student
     @id, @name, @grade = id, name, grade
   end
 
+  def self.create_table
+    sql = <<-SQL 
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT, 
+        grade TEXT
+      )
+    SQL
+    DB[:conn].execute(sql)
+  end
 
 end

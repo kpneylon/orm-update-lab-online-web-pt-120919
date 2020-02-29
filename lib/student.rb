bea07a59cb008bce7184efd7f5c7162b7fc962d2
Kenneth Neylon <kenneth_neylon@hotmail.com>
2020-02-29 02:59:09 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,6 +3,10 @@ require_relative "../config/environment.rb"
 class Student
 
   attr_accessor :id, :name, :grade
+  
+  def initialize(id=nil, name, grade)
+    @id, @name, @grade = id, name, grade
+  end
 
 
 end
